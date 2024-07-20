6aa8f606b98b9ed32d1398b1bfe16578b258bd55
Fxmouskid <iyan005@outlook.com>
2024-07-20 20:41:12 +0300

    Subject : Adding header file with a few included libraries
    What :
    
    Why :
    
    How :
    
    Footer :

---
+++ includes/philosopher.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosopher.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/18 21:06:01 by inazaria          #+#    #+#             */
+/*   Updated: 2024/07/19 16:28:38 by inazaria         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHER_H
+# define PHILOSOPHER_H
+
+# include <sys/time.h>
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+
+
+#endif
